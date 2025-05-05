2be7f5ce29d2c08603cb9c0ec962494012aa0b50
Ritika Chhoker <158747135+ritikachhoker@users.noreply.github.com>
2025-05-05 22:56:24 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const AppContext = createContext()
 
 const AppContextProvider =  (props) =>{
     const currencySymbol = '$'
-    const backendUrl = "https://swastikcare-oon7.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [doctors, setDoctors]= useState([])
 
      const [token , setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
